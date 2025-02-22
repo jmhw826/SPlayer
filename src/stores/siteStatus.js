3b4ce8dc152452de4fd7db2b8a20139e8d0e1dd6
IamFurina <emotionui@qq.com>
2025-02-22 21:32:39 +0800

    Patch: 开始往SP本体加入Unblock

--- src/stores/siteStatus.js
+++ src/stores/siteStatus.js
@@ -5,7 +5,7 @@ const useSiteStatusStore = defineStore("siteStatus", {
   state: () => {
     return {
       // 菜单折叠状态
-      asideMenuCollapsed: false,
+      asideMenuCollapsed: true,
       // 搜索框状态
       searchInputFocus: false,
       // 是否展示播放控制条
