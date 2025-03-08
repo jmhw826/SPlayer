e510cf5766a6dbbe7408e47eb4d677565d138e8c
IamFurina <186459674+IamFurina@users.noreply.github.com>
2025-03-08 15:42:59 +0800

    Fix: 修复换行问题

--- src/components/Modal/Settings.vue
+++ src/components/Modal/Settings.vue
@@ -108,7 +108,7 @@ defineExpose({ showModal: openModal });
 <style lang="scss" scoped>
 .settings-modal {
   display: flex;
-  flex-direction: row;
+  flex-direction: warp;
   .tabs-container {
     width: 200px;
     border-right: 1px solid #e0e0e0;
