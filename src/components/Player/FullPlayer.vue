1c43d454e377e0910219c0fe2254222714414662
IamFurina <186459674+IamFurina@users.noreply.github.com>
2025-04-12 13:49:01 +0800

    style. 修复标签

--- src/components/Player/FullPlayer.vue
+++ src/components/Player/FullPlayer.vue
@@ -751,7 +751,7 @@ onUnmounted(() => {
       }
       &.pure {
         width: 100%;
-        height: calc(100vh - 290px);
+        height: calc(100vh - 210px);
         display: flex;
         flex-direction: column;
         align-items: center;
