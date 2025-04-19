d41432214a5f8ef812f17f114e8bbdea5f6dce62
IamFurina <emotionui@qq.com>
2025-04-19 14:36:44 +0800

    fix. 停止使用同根域名

--- src/api/song.js
+++ src/api/song.js
@@ -55,7 +55,7 @@ export const getSongUrl = (id, level = "standard") => {
  */
 export const getMusicNumUrl = async (id) => {
   const server = "pyncmd,kuwo";
-  if (import.meta.env["RENDERER_VITE_SITE_ROOT"] === "true" || import.meta.env["RENDERER_VITE_SITE_ROOT"] === true) {
+  if (import.meta.env["RENDERER_VITE_SITE_ROOT"] === "true") {
     var unmurl = "/unblock/";
   } else {
     var unmurl = `${import.meta.env.VITE_UNM_API}`;
