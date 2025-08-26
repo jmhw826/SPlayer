633f1a43b0f2d8d343aee493d19e39ed1adfa05e
ImFurina <222616389+MoeFurina@users.noreply.github.com>
2025-08-26 16:44:48 +0800

    ✨feat(cookielogin): 支持electron网页端获取cookie(不稳定)

--- src/components/Modal/Login/LoginCookie.vue
+++ src/components/Modal/Login/LoginCookie.vue
@@ -7,7 +7,7 @@
     </n-alert>
     <n-input v-model:value="cookie" :autosize="{ minRows: 3, maxRows: 6 }" type="textarea" placeholder="请输入 Cookie" />
     <n-flex class="menu">
-      <n-button type="primary" v-if="isWeb" @click="openWeb">官网登陆</n-button>
+      <n-button type="primary" @click="openWeb">打开官方登陆页面</n-button>
       <n-button type="primary" @click="login">登录</n-button>
     </n-flex>
   </div>
@@ -19,6 +19,18 @@ import { checkPlatform } from "@/utils/helper";
 const isWeb = !checkPlatform.electron();
 const cookie = ref("");
 const emit = defineEmits(["setLoginData"]);
+
+// 开启窗口
+const openElectronBrowser = () => {
+  window.$dialog.info({
+    title: "使用前告知",
+    content:
+      "请知悉，该功能仍旧无法确保账号的安全性！请自行决定是否使用！如遇打开窗口后页面出现白屏或者无法点击等情况，请关闭后重试",
+    positiveText: "我已了解",
+    negativeText: "取消",
+    onPositiveClick: () => electron.ipcRenderer.send("open-login-web"),
+  });
+};
 
 // Cookie 登录
 const login = async () => {
@@ -46,11 +58,25 @@ const login = async () => {
 };
 
 const openWeb = () => {
-  $message.warning("功能开发中!");
-  /*
-  window.open("https://music.163.com/", "_blank");
-  */
+  if (isWeb) {
+    $message.warning("功能开发中!");
+    // window.open("https://music.163.com/", "_blank");
+  } else {
+    openElectronBrowser();
+  }
 };
+
+onMounted(() => {
+  if (!isWeb) {
+    electron.ipcRenderer.on("send-cookies", (_, value) => {
+      if (!value) return;
+      cookie.value = value;
+      // 测试用
+      // cookie.value = "MUSIC_U=1eb9ce22024bb666e99b6743b2222f29ef64a9e88fda0fd5754714b900a5d70d993166e004087dd3b95085f6a85b059f5e9aba41e3f2646e3cebdbec0317df58c119e5;";
+      login();
+    });
+  }
+});
 
 </script>
 
